9d5eda4440123ea85a5ded032db239cf5b97e470
Domnic Amalan <amalandomnic@gmail.com>
2021-09-22 07:55:49 +0530

    Fix

--- pages/api/user/[id]/index.js
+++ pages/api/user/[id]/index.js
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const sampleData = {
+  "0": {
+    name: "Hoo",
+    desc: "He is doctor"
+  },
   "1": {
     name: "John",
     desc: "He is engineer"
@@ -9,14 +13,8 @@ const sampleData = {
     name: "Doe",
     desc: "He is doctor"
   },
-  "0": {
-    name: "Hoo",
-    desc: "He is doctor"
-  }
 }
 
 export default function handler(req, res) {
-  console.log(req.query.id)
-  console.log(sampleData[req.query.id])
   res.status(200).json(sampleData[req.query.id])
 }
